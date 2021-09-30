aec124710f9797da689af253e40a101fad831794
mbkranz <kranz-michael@norc.org>
2021-09-29 20:56:24 -0500

    added argument options

--- upload_data.sh
+++ upload_data.sh
@@ -1,12 +1,61 @@
 #!/bin/bash
+
+ENDPOINT='https://jcoin.datacommons.io'
 OEPS_REPO='/mnt/c/Users/kranz-michael/projects/opioid-policy-scan/data_final'
-CREDENTIALS_PATH='/mnt/c/Users/kranz-michael/Downloads/credentials.json'
-#ENDPOINT='https://qa-jcoin.planx-pla.net'
-ENDPOINT='https://jcoin.datacommons.io/'
+CREDENTIALS_PATH='credientials.json'
 
-./gen3-client configure --profile=jdc --cred=$CREDENTIALS_PATH --apiendpoint=$ENDPOINT
+# if no arguments are provided, return usage function
+#MBK: no usage fxn so commented out for now
+if [ $# -eq 0 ]; then
+    #usage # run usage function
+    #exit 1
+    echo 'No commands but we have defaults'
+    echo ENDPOINT: $ENDPOINT
+    echo OEPS_REPO: $OEPS_REPO
+    echo CREDENTIALS_PATH: $CREDENTIALS_PATH
+fi
 
-./gen3-client upload --profile=jdc --upload-path=$OEPS_REPO
+for arg in "$@"; do
+    case $arg in
+        -d | --data-dir)
+            OEPS_REPO=$2
+            shift # shift removes argument from from `$@`
+            shift # 2nd shift remove value
+            ;;
+        -c | --credentials-path)
+            CREDENTIALS_PATH=$2
+            shift # Remove argument (-t) name from `$@`
+            shift # Remove argument value (latest) from `$@`
+            ;;
+        -u | --data-commons-url)
+            ENDPOINT=$2
+            shift # Remove argument (-t) name from `$@`
+            shift # Remove argument value (latest) from `$@`
+            ;;
+        -m | --update-metadata)
+            is_metadata=true
+            shift # Remove argument (-t) name from `$@`
+            ;;
+        -r | --update-data)
+            is_data=true
+            shift # Remove argument (-t) name from `$@`
+            ;;
+        -h | --help)
+            #usage # run usage function on help
+            ;;
+        *)
+            #usage # run usage function if wrong argument provided
+            ;;
+    esac
+done
+
+if [[ $is_data ]]; then
+    ./gen3-client configure --profile=jdc --cred=$CREDENTIALS_PATH --apiendpoint=$ENDPOINT
+
+    ./gen3-client upload --profile=jdc --upload-path=$OEPS_REPO
+else
+    echo "Data upload not selected"
+fi
 
 
 # oeps_files=$(find $OEPS_REPO)
@@ -16,3 +65,14 @@ ENDPOINT='https://jcoin.datacommons.io/'
 #     ./gen3-client upload --profile=jdc --upload-path=$f
 
 # done
+
+
+if [[ $is_metadata ]]; then
+    python update_metadata.py \
+    --endpoint $ENDPOINT \
+    --program 'JCOIN' \
+    --project 'OEPS' \
+     --credentials $CREDENTIALS_PATH
+else
+    echo "Metadata update not selected"
+fi
